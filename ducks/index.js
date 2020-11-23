bac384f4718e04a82fa987de5178c1fff0646473
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-23 22:06:18 +0000

    :sparkles: create rotReducer

---
+++ ducks/index.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux';
+
+
+const appReducer = combineReducers({
+    // Include reducers here
+})
+
+export const rootReducer = (state, action) => {
+    if (action.type === 'auth/logOut') {
+        // Delete redux persisted state from async storage
+        AsyncStorage.removeItem('persist:root')
+
+        state = undefined
+    }
+
+    return appReducer(state, action)
+}
