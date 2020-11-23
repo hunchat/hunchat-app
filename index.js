73049df59cc86fe59d165eb1f91edbd081aa23f7
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-23 21:57:09 +0000

    :wrench: change app entrypoint to index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import { registerRootComponent } from 'expo';
+
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store';
+
+import App from './App';
+
+
+class Root extends React.Component {
+    render(){
+      return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+      )
+
+    }
+
+}
+
+
+// Set as root component
+registerRootComponent(Root);
