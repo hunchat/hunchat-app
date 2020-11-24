511e9522a20abd287eab673e43481261d7a85ecc
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-24 15:28:25 +0000

    :lipstick: add SafeAreaView to App

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
 
 import AppStack from './navigators/AppStackNavigator';
 import BottomTabs from './navigators/BottomTabs';
 import FeedStack from './navigators/FeedStackNavigator';
 import { navigationRef } from './navigators/RootNavigation';
 
-export default function App() {
+function App() {
   return (
-    <NavigationContainer ref={navigationRef}>
-      <AppStack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true }}>
-        <AppStack.Screen name='BottomTabs' component={BottomTabs} />
-        <AppStack.Screen name='FeedStack' component={FeedStack} />
-      </AppStack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer ref={navigationRef}>
+        <AppStack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true }}>
+          <AppStack.Screen name='BottomTabs' component={BottomTabs} />
+          <AppStack.Screen name='FeedStack' component={FeedStack} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight ,
+  }
+})
+
+export default App;
