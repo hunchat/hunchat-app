89c768745733aa08b29bebb8c06b4415b46c6812
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-24 22:02:43 +0000

    :lipstick: improve VideoPreview styles

--- components/Video/VideoPreview.js
+++ components/Video/VideoPreview.js
@@ -1,30 +1,67 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
 
 const spacing = 15;
 const width = (Dimensions.get('window').width - 4 * 15) / 2;
 
+const authorUsernameMaxCharacters = 13;
+
 
 function VideoPreview({
   id,
-  title,
+  description,
   author,
   navigation,
 }) {
   return (
     <View style={styles.container}>
+      <View style={styles.author}>
+        <Image source={Object({uri: author.imageUrl})} style={styles.authorImage} />
+        <Text style={styles.authorUsername}>
+          { ((author.username).length > authorUsernameMaxCharacters) ?
+              (((author.username).substring(0, authorUsernameMaxCharacters-3)) + '...') :
+              author.username}
+        </Text>
+      </View>
+      <Text style={styles.description} numberOfLines={3} ellipsizeMode="tail">
+        {description}
+      </Text>
     </View>
   )
 }
 
 const styles = {
   container: {
-    backgroundColor: 'red',
+    backgroundColor: '#d3d3d3',
     height: 250,
     width: width,
     margin: spacing,
+    padding: 15,
     borderRadius: 15,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  author: {
+    flexDirection: 'row',
+    marginVertical: 3,
+  },
+  authorImage: {
+    backgroundColor: 'white',
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    borderWidth: 3,
+  },
+  authorUsername: {
+    marginHorizontal: 5,
+    textAlignVertical: 'bottom',
+    numberOfLines: 1,
+    color: 'white',
+  },
+  description: {
+    fontSize: 14,
+    numberOfLines: 3,
   }
 }
 
