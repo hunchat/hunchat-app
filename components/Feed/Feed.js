72e6e0d2400490f46415b92adfe7b6d063baa8a3
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-27 22:14:11 +0000

    :construction: update Feed

--- components/Feed/Feed.js
+++ components/Feed/Feed.js
@@ -1,38 +1,91 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  StatusBar
+ } from 'react-native';
 
 import { Video } from '../Video';
-
-
-const video = {
-  id: '1',
-  description: 'This is a very short description of this video I just posted',
-  author: {
-    username: '@JaneFisher',
-    imageUrl: ''
-  },
-  externalLink: 'example.com',
-  views: '306',
-  likes: '189',
-  shares: '22',
-  answer: '1',
-}
+import ViewPager from '../ViewPager';
 
 
 function Feed({
   navigation,
 }) {
+  const renderItem = ({ item }) => {
+    return <Video key={item.id} {...item} navigation={navigation}/>
+  };
+
+  const keyExtractor = item => item.id;
+
   return (
     <View style={styles.container}>
-      <Video {...video} navigation={navigation}/>
+      <ViewPager
+        ref={ref => this.viewPager = ref}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.viewPager}
+        size={size}
+        horizontal={false}
+      />
     </View>
   )
 }
 
+const data = [
+  {
+    id: '1',
+    description: 'This is a very short description of this video I just posted',
+    author: {
+      username: '@JaneFisher',
+      imageUrl: ''
+    },
+    externalLink: 'example.com',
+    views: '306',
+    likes: '189',
+    shares: '22',
+    answer: '1',
+  },
+  {
+    id: '2',
+    description: 'This is a very short description of this video I just posted',
+    author: {
+      username: '@JonDoe',
+      imageUrl: ''
+    },
+    externalLink: 'example.com',
+    views: '200',
+    likes: '81',
+    shares: '12',
+    answer: '5',
+  },
+  {
+    id: '3',
+    description: 'This is a very short description of this video I just posted',
+    author: {
+      username: '@JaneDoe',
+      imageUrl: ''
+    },
+    externalLink: 'example.com',
+    views: '404',
+    likes: '121',
+    shares: '21',
+    answer: '26',
+  },
+]
+
+const size = Dimensions.get('window').height - StatusBar.currentHeight;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
+  viewPager: {
+    flex: 1,
+  },
 })
 
 export default Feed;
