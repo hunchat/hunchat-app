8482aa805b9b841ea8859e1a68beed0822c525a9
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-02 22:20:20 +0000

    :sparkles: add video selectors

--- ducks/videosSlice.js
+++ ducks/videosSlice.js
@@ -2,16 +2,148 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { createSelector } from 'reselect';
 
+import { getUsers } from './usersSlice';
+
 
 export const getVideos = state => state.videos.byId;
 export const getVideoId = (state, props) => props.videoId;
+
+export const makeGetVideoAuthorId = () => {
+  return createSelector(
+    [getVideos, getVideoId],
+    (videos, id) => {
+      return videos[id].author;
+    }
+  )
+};
+
+export const makeGetVideoAuthor = () => {
+  const getVideoAuthorId = makeGetVideoAuthorId();
+  return createSelector(
+    [getUsers, getVideoAuthorId],
+    (users, id) => {
+      return {
+        id: id,
+        username: users[id].username,
+        imageUrl: users[id].imageUrl,
+      }
+    }
+  )
+};
+
+export const makeGetVideo = () => {
+  const getVideoAuthor = makeGetVideoAuthor();
+  return createSelector(
+    [getVideos, getVideoId, getVideoAuthor],
+    (videos, id, author) => {
+      return {
+        id: id,
+        url: videos[id].url,
+        description: videos[id].description,
+        author: author,
+        externalLink: videos[id].externalLink,
+        views: videos[id].views,
+        likes: videos[id].likes,
+        shares: videos[id].shares,
+        answer: videos[id].answer,
+      }
+    }
+  )
+};
 
 
 const videosSlice = createSlice({
   name: 'videos',
   initialState: {
     byId: {
-
+      '1': {
+        id: '1',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/128422991_423213609066129_6831456051227847080_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=EGKCGPJF10UAX-4fYyG&oe=5FC73F22&oh=cd22acb05b17164ab1575dcaceddf1a4',
+        description: 'This is a very short description of this video I just posted',
+        author: 'aa',
+        externalLink: 'example.com',
+        views: '306',
+        likes: '189',
+        shares: '22',
+        answer: '1',
+      },
+      '2': {
+        id: '2',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/126888578_378596023561203_3448226840820167123_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=UKyFzd2USFEAX8gUXWH&oe=5FC86C91&oh=0cd0a03647a5977d9c190c6a150213a0',
+        description: 'Secret beach “La Playa del Amor” in Islas Marietas, Mexico',
+        author: 'bb',
+        externalLink: 'example.com',
+        views: '200',
+        likes: '81',
+        shares: '12',
+        answer: '5',
+      },
+      '3': {
+        id: '3',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/125869848_185885116450220_5010921192452842805_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=Vd4n_C7i62kAX-3rgtq&oe=5FC8BC91&oh=a4657441aa561aed653ef945872e27c0',
+        description: 'This was CLEAN',
+        author: 'cc',
+        externalLink: 'example.com',
+        views: '404',
+        likes: '121',
+        shares: '21',
+        answer: '26',
+      },
+      '4': {
+        id: '4',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/127199917_913327152530243_4641490979170110651_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=KVNILaZP4YgAX8v_36O&oe=5FC86BE2&oh=ae4c6440cbcedcad60c3f145adc86239',
+        description: 'Persistance is key',
+        author: 'dd',
+        externalLink: 'example.com',
+        views: '404',
+        likes: '121',
+        shares: '21',
+        answer: '26',
+      },
+      '5': {
+        id: '5',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/125505221_195609342034245_3388236763194820638_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=LaDTAsF_DIEAX91n5ft&oe=5FC8949C&oh=8433aae9890375a055a430a011cc1957',
+        description: 'I can be a ball boy too',
+        author: 'ee',
+        externalLink: 'example.com',
+        views: '404',
+        likes: '121',
+        shares: '21',
+        answer: '26',
+      },
+      '6': {
+        id: '6',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/127939334_198393301814256_8921741831673252662_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=LLbJ2DE5IRMAX8HAX9g&oe=5FC8F656&oh=d429e7aa3827e84459ac4f6a78f9a5a3',
+        description: 'Free diving in crystal clear water at the only place on Earth where you can touch both the North American and the Eurasian Continental Plate at the same time! Location: Silfra, Iceland',
+        author: 'ff',
+        externalLink: 'example.com',
+        views: '404',
+        likes: '121',
+        shares: '21',
+        answer: '26',
+      },
+      '7': {
+        id: '7',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/125549710_713340066274556_5872930227307495258_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=5eC5hEO_LZYAX8ik57V&oe=5FC8AC55&oh=311b4a96543ed3c28d37498826b40be5',
+        description: 'Me at the Olympics',
+        author: 'gg',
+        externalLink: 'example.com',
+        views: '404',
+        likes: '121',
+        shares: '21',
+        answer: '26',
+      },
+      '8': {
+        id: '8',
+        url: 'https://instagram.flis5-1.fna.fbcdn.net/v/t50.2886-16/126435293_306716230400845_5494035644464095985_n.mp4?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=Bti1NjEUTZwAX_z-kqi&oe=5FC88A1F&oh=1575d32435cde6b7c5e551df62858df6',
+        description: 'Who’d love to meet this beautiful waving bear? 😍🐻',
+        author: 'hh',
+        externalLink: 'example.com',
+        views: '404',
+        likes: '121',
+        shares: '21',
+        answer: '26',
+      },
     }
   },
   reducers: {
