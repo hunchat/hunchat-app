d7d756f1f50b8316be36f3328f059a4713bd0198
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-02 23:06:33 +0000

    :sparkles: create VideoStack

---
+++ navigators/VideoStackNavigator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
+
+import FeedScreen from '../screens/FeedScreen';
+import ListScreen from '../screens/ListScreen';
+import VideoScreen from '../screens/VideoScreen';
+
+
+const Stack = createSharedElementStackNavigator();
+
+const VideoStack = () => (
+  <Stack.Navigator
+    screenOptions={{
+      gestureEnabled: false,
+      headerShown: false,
+      cardOverlayEnabled: true,
+      cardStyle: { backgroundColor: "transparent" },
+    }}
+    mode="modal"
+  >
+    <Stack.Screen name="List" component={ListScreen} />
+    <Stack.Screen
+      name="Video"
+      component={VideoScreen}
+      sharedElements={(route) => {
+        return [route.params.video.id]
+      }}
+    />
+  </Stack.Navigator>
+);
+
+export default VideoStack;
