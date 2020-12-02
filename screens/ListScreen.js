f3a7313dea639e874f02b89942bcaec24cc62dcc
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-02 23:07:03 +0000

    :art: improve ListScreen code structure

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { Header } from '../components/Header';
 import { VideosList } from '../components/Video';
 
+import { makeGetList } from '../ducks/listsSlice';
+
 
 class ListScreen extends React.Component {
   constructor(props) {
@@ -15,27 +17,29 @@ class ListScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Header title={this.props.name} navigation={this.props.navigation}/>
-      <VideosList navigation={this.props.navigation}/>
+        <Header title={this.props.name} navigation={this.props.navigation}/>
+        <VideosList videosIds={this.props.videos} navigation={this.props.navigation}/>
       </View>
     )
   }
-}
+};
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   }
-})
+});
 
-const makeMapStateToProps = () => {
-  const mapStateToProps = (state, ownProps) => {
+const makeMapStateToProps = (state) => {
+  const getList = makeGetList();
+  return function mapStateToProps(state, ownProps) {
+    let list = getList(state, { listId: ownProps.route.params.list.id });
     return {
-      name: ownProps.route.params.list.name,
+      name: list.name,
+      videos: list.videos,
     }
   }
-  return mapStateToProps
 }
 
 
