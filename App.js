d225681ad94ad0fddf810939b577bab23f28e250
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-03 23:18:37 +0000

    :sparkles: use new navigation

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
-import AddVideoStack from './navigators/AddVideoStackNavigator';
-import AppStack from './navigators/AppStackNavigator';
-import BottomTabs from './navigators/BottomTabs';
-import ExploreStack from './navigators/ExploreStackNavigator';
-import FeedStack from './navigators/FeedStackNavigator';
-import ListStack from './navigators/ListStackNavigator';
-import VideoStack from './navigators/VideoStackNavigator';
-import { navigationRef } from './navigators/RootNavigation';
+import {
+  AddVideoStack,
+  verticalAnimation,
+  AppStack,
+  BottomTabs,
+  ExploreStack,
+  FeedStack,
+  ListStack,
+  VideoStack,
+  navigationRef,
+} from "./navigation";
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer ref={navigationRef}>
-        <AppStack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true }}>
-          <AppStack.Screen name='BottomTabs' component={BottomTabs} />
-          <AppStack.Screen name='AddVideoStack' component={AddVideoStack} />
-          <AppStack.Screen name='ExploreStack' component={ExploreStack} />
-          <AppStack.Screen name='FeedStack' component={FeedStack} />
-          <AppStack.Screen name='ListStack' component={ListStack} />
-          <AppStack.Screen name='VideoStack' component={VideoStack} />
+        <AppStack.Navigator
+          screenOptions={{ headerShown: false, gestureEnabled: true }}
+        >
+          <AppStack.Screen name="BottomTabs" component={BottomTabs} />
+          <AppStack.Screen name="AddVideoStack" component={AddVideoStack} options={verticalAnimation}/>
+          <AppStack.Screen name="ExploreStack" component={ExploreStack} />
+          <AppStack.Screen name="FeedStack" component={FeedStack} />
+          <AppStack.Screen name="ListStack" component={ListStack} />
+          <AppStack.Screen name="VideoStack" component={VideoStack} />
         </AppStack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
@@ -32,7 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight,
-  }
-})
+  },
+});
 
 export default App;
