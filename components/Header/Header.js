1c17a6f99f87753cea508fb497139c23200f8256
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-03 23:22:16 +0000

    :lipstick: change Header to dark mode

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import { Icon } from 'react-native-elements';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
 
+export const HEADER_HEIGHT = 50;
 
-function Header({
-  title,
-  navigation,
-}) {
+function Header({ title }) {
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>{title}</Text>
       </View>
       <View style={styles.navigation}>
-        <Icon name="person-outline" color="black" size={40} />
-        <Icon name="email-outline" type="material-community" color="black" size={40} />
+        <Icon name="person-outline" color="white" size={35} />
+        <Icon
+          name="email-outline"
+          type="material-community"
+          color="white"
+          size={35}
+        />
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: HEADER_HEIGHT,
     paddingVertical: 6,
     paddingHorizontal: 20,
-    backgroundColor: 'white',
+    backgroundColor: "black",
+    elevation: 4,
+    zIndex: 100,
   },
   titleContainer: {
     flex: 3,
+    justifyContent: "flex-end",
   },
   title: {
     fontSize: 24,
+    fontWeight: "700",
+    color: "white",
   },
   navigation: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  }
-})
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+});
 
 export default Header;
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,3 @@
-import Header from './Header';
+import Header, { HEADER_HEIGHT } from "./Header";
 
-export {
-  Header,
-}
+export { Header, HEADER_HEIGHT };
