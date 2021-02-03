8a84beb4a2edd56822de5d13f9b619ac50459988
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-03 23:27:32 +0000

    :construction: update ExploreScreen

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,33 +1,71 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { withNavigation } from 'react-navigation';
+import React from "react";
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { withNavigation } from "react-navigation";
 
-import { Header } from '../components/Header';
-import { SearchBar } from '../components/Search';
-import { VideosList } from '../components/Video';
+import { Header } from "../components/Header";
+import { AddVideoButton } from "../components/AddVideo";
+import { ExploreListsSelector, EXPLORE_LISTS_SELECTOR_HEIGHT } from "../components/Explore";
+import { VideosList } from "../components/Video";
 
+function ExploreScreen({ navigation }) {
+  const scrollY = new Animated.Value(0);
+  const diffClamp = Animated.diffClamp(scrollY, 0, EXPLORE_LISTS_SELECTOR_HEIGHT);
+  const translateY = diffClamp.interpolate({
+    inputRange: [0, EXPLORE_LISTS_SELECTOR_HEIGHT],
+    outputRange: [0, -EXPLORE_LISTS_SELECTOR_HEIGHT],
+    extrapolate: "clamp"
+  });
 
-class ExploreScreen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  const handleScroll = (e) => {
+    scrollY.setValue(e.nativeEvent.contentOffset.y)
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header title={"Lista Geral"} navigation={this.props.navigation}/>
-        <SearchBar />
-        <VideosList navigation={this.props.navigation}/>
-      </View>
-    )
-  }
+  return (
+    <View style={styles.container}>
+      <Header title={"Explore"} />
+      <Animated.View
+        style={{
+          transform: [
+            { translateY },
+            { perspective: 1000 }
+          ],
+          elevation: 3,
+          zIndex: 80,
+        }}
+      >
+        <ExploreListsSelector selectedList="All topics"/>
+      </Animated.View>
+      <Animated.View style={{ flex: 20000 }}>
+        <VideosList
+          videosIds={["1", "2", "3", "4", "5"]}
+          handleScroll={Animated.event(
+            [{
+              nativeEvent: {
+                contentOffset: {
+                  y: scrollY
+                 }
+              }
+            }],
+            {
+              useNativeDriver: true,
+              listener: event => {
+                handleScroll(event);
+              }
+            }
+          )}
+          marginTop={EXPLORE_LISTS_SELECTOR_HEIGHT}
+        />
+      </Animated.View>
+      <AddVideoButton />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-  }
-})
+    backgroundColor: "black",
+  },
+});
 
 export default withNavigation(ExploreScreen);
