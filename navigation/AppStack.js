ba15b96a3626d55fdf3bfdc49de055615d7b1bb2
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-04 23:11:21 +0000

    :art::truck: move AppStack.Navigator screens to AppStack file

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,5 +1,45 @@
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-const AppStack = createStackNavigator();
+import AddVideoStack from "./AddVideoStack";
+import { horizontalAnimation, verticalAnimation } from "./animations";
+import AuthStack from "./AuthStack";
+import BottomTabs from "./BottomTabs";
+import ExploreStack from "./ExploreStack";
+import FeedStack from "./FeedStack";
+import HeaderStack from "./HeaderStack";
+import ListStack from "./ListStack";
+import VideoStack from "./VideoStack";
+import { store } from "../store";
+
+
+const Stack = createStackNavigator();
+
+const AppStack = () => {
+  const state = store.getState();
+
+  const authRefreshToken = state.auth.refreshToken;
+  const currentUserId = state.users.currentUserId;
+
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, gestureEnabled: true }}
+    >
+      {authRefreshToken == null ||
+      currentUserId == null ? (
+        <Stack.Screen name="AuthStack" component={AuthStack} />
+      ) : (
+        <>
+        <Stack.Screen name="BottomTabs" component={BottomTabs} />
+        <Stack.Screen name="AddVideoStack" component={AddVideoStack} options={verticalAnimation}/>
+        <Stack.Screen name="ExploreStack" component={ExploreStack} />
+        <Stack.Screen name="FeedStack" component={FeedStack} />
+        <Stack.Screen name="ListStack" component={ListStack} />
+        <Stack.Screen name="VideoStack" component={VideoStack} />
+        </>
+      )}
+    </Stack.Navigator>
+  )
+};
 
 export default AppStack;
