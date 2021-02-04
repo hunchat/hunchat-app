9a6c58530654e903e011e4e459d2df48f818cee5
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-04 23:13:54 +0000

    :art: move AppStack navigator screens from App.js

--- App.js
+++ App.js
@@ -2,32 +2,13 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
-import {
-  AddVideoStack,
-  verticalAnimation,
-  AppStack,
-  BottomTabs,
-  ExploreStack,
-  FeedStack,
-  ListStack,
-  VideoStack,
-  navigationRef,
-} from "./navigation";
+import { AppStack, navigationRef } from "./navigation";
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer ref={navigationRef}>
-        <AppStack.Navigator
-          screenOptions={{ headerShown: false, gestureEnabled: true }}
-        >
-          <AppStack.Screen name="BottomTabs" component={BottomTabs} />
-          <AppStack.Screen name="AddVideoStack" component={AddVideoStack} options={verticalAnimation}/>
-          <AppStack.Screen name="ExploreStack" component={ExploreStack} />
-          <AppStack.Screen name="FeedStack" component={FeedStack} />
-          <AppStack.Screen name="ListStack" component={ListStack} />
-          <AppStack.Screen name="VideoStack" component={VideoStack} />
-        </AppStack.Navigator>
+        <AppStack />
       </NavigationContainer>
     </SafeAreaView>
   );
