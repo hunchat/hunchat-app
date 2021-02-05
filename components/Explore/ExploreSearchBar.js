1e98a7d24284d9d50e4d9dddb61995c26842fdc8
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 12:04:07 +0000

    :sparkles: create ExploreSearchBar

---
+++ components/Explore/ExploreSearchBar.js
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+  Pressable,
+} from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+const SEARCH_BAR_PADDING_VERTICAL = 10;
+const ICON_SIZE = 24;
+export const SEARCH_BAR_HEIGHT = 2*SEARCH_BAR_PADDING_VERTICAL + ICON_SIZE
+
+const { width } = Dimensions.get("window");
+
+const ExploreSearchBar = () => {
+  const textInput = useRef(null);
+  const [value, onChangeText] = useState("");
+
+  function handlePress() {
+    textInput.current.focus();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Pressable
+        onPress={handlePress}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? "black"
+              : "black"
+          },
+          styles.searchBar
+        ]}
+      >
+        <Ionicons name="ios-search-outline" size={ICON_SIZE} color={"grey"} />
+        <TextInput
+          ref={textInput}
+          style={styles.textInput}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          placeholder="Type to explore new topics..."
+          placeholderTextColor={"grey"}
+        />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "black",
+    paddingHorizontal: 5,
+    justifyContent: "center",
+  },
+  searchBar: {
+    borderRadius: 10,
+    paddingVertical: SEARCH_BAR_PADDING_VERTICAL,
+    paddingHorizontal: 5,
+    flexDirection: "row",
+  },
+  textInput: {
+    marginLeft: 5,
+    color: "white",
+  },
+});
+export default ExploreSearchBar;
--- components/Explore/index.js
+++ components/Explore/index.js
@@ -1,6 +1,9 @@
 import ExploreListsSelector, { EXPLORE_LISTS_SELECTOR_HEIGHT } from "./ExploreListsSelector";
+import ExploreSearchBar, { SEARCH_BAR_HEIGHT } from "./ExploreSearchBar";
 
 export {
   ExploreListsSelector,
-  EXPLORE_LISTS_SELECTOR_HEIGHT
+  EXPLORE_LISTS_SELECTOR_HEIGHT,
+  ExploreSearchBar,
+  SEARCH_BAR_HEIGHT,
 };
