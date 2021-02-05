5ed2d5034648e3e85d3d047c7fe499b227c94786
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 12:01:20 +0000

    :construction: update ListsScreen

--- screens/ListsScreen.js
+++ screens/ListsScreen.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { withNavigation } from 'react-navigation';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { withNavigation } from "react-navigation";
 
-import { Header } from '../components/Header';
-import { Lists } from '../components/Lists';
-
+import { AddVideoButton } from "../components/AddVideo";
+import { Header } from "../components/Header";
+import { Lists } from "../components/Lists";
 
 class ListsScreen extends React.Component {
   constructor(props) {
@@ -14,17 +14,18 @@ class ListsScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header title={"Lista Listas"} navigation={this.props.navigation}/>
+        <Header title={"Your Lists"} navigation={this.props.navigation} />
         <Lists navigation={this.props.navigation} />
+        <AddVideoButton />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
-})
+  },
+});
 
 export default withNavigation(ListsScreen);
