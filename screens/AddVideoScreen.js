30333a8a6c4408540a62d07281988279a0795159
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 11:58:59 +0000

    :construction: update AddVideoScreen

--- screens/AddVideoScreen.js
+++ screens/AddVideoScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { withNavigation } from 'react-navigation';
+import React from "react";
+import { connect } from "react-redux";
+import { withNavigation } from "react-navigation";
 import {
   View,
   StyleSheet,
@@ -8,10 +8,9 @@ import {
   TextInput,
   TouchableOpacity,
   Dimensions,
- } from 'react-native';
-import { Video } from 'expo-av';
-import { Icon } from 'react-native-elements';
-
+} from "react-native";
+import { Video } from "expo-av";
+import { Icon } from "react-native-elements";
 
 class AddVideoScreen extends React.Component {
   constructor(props) {
@@ -26,11 +25,11 @@ class AddVideoScreen extends React.Component {
   }
 
   onChangeCaptionTextInput(text) {
-    this.setState({ caption: text })
+    this.setState({ caption: text });
   }
 
   onChangeLinkTextInput(text) {
-    this.setState({ link: text })
+    this.setState({ link: text });
   }
 
   render() {
@@ -44,7 +43,7 @@ class AddVideoScreen extends React.Component {
         <View style={styles.preview}>
           <Text style={styles.previewText}>Preview</Text>
           <Video
-            source={{uri: this.props.uri}}
+            source={{ uri: this.props.uri }}
             rate={1.0}
             volume={1.0}
             isMuted={false}
@@ -59,32 +58,41 @@ class AddVideoScreen extends React.Component {
             <Text>Add tags</Text>
           </View>
           <View style={styles.addCaptionContainer}>
-            <Text >Add caption</Text>
+            <Text>Add caption</Text>
             <TextInput
-              ref={(input) => {this.captionTextInput = input;}}
-              textContentType='none'
+              ref={(input) => {
+                this.captionTextInput = input;
+              }}
+              textContentType="none"
               value={this.state.caption}
-              onChangeText={text => this.onChangeCaptionTextInput(text)}
-              onSubmitEditing={() => { this.linkTextInput.focus(); }}
+              onChangeText={(text) => this.onChangeCaptionTextInput(text)}
+              onSubmitEditing={() => {
+                this.linkTextInput.focus();
+              }}
             />
           </View>
           <View style={styles.linkResourcesContainer}>
             <Text>Link resources</Text>
             <TextInput
-              ref={(input) => {this.linkTextInput = input;}}
-              textContentType='URL'
+              ref={(input) => {
+                this.linkTextInput = input;
+              }}
+              textContentType="URL"
               value={this.state.link}
-              onChangeText={text => this.onChangeLinkTextInput(text)}
+              onChangeText={(text) => this.onChangeLinkTextInput(text)}
             />
           </View>
         </View>
         <View style={styles.bottomBar}>
-          <TouchableOpacity style={styles.sendButton} onPress={this.onPressRightArrow}>
+          <TouchableOpacity
+            style={styles.sendButton}
+            onPress={this.onPressRightArrow}
+          >
             <Text style={styles.sendButtontext}>Send</Text>
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
@@ -94,18 +102,18 @@ const styles = StyleSheet.create({
   },
   header: {
     top: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     height: 50,
     paddingVertical: 6,
     paddingHorizontal: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   preview: {
     flex: 1,
   },
   previewText: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   video: {
     flex: 1,
@@ -114,43 +122,33 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 30,
   },
-  addTagsContainer: {
-
-  },
-  addCaptionContainer: {
-
-  },
-  linkResourcesContainer: {
-
-  },
+  addTagsContainer: {},
+  addCaptionContainer: {},
+  linkResourcesContainer: {},
   bottomBar: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: Dimensions.get('window').height * 0.1,
-    backgroundColor: '#e9e9e9',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: Dimensions.get("window").height * 0.1,
+    backgroundColor: "#e9e9e9",
     paddingHorizontal: 10,
   },
   sendButton: {
-    height: '70%',
-    width: Dimensions.get('window').width * 0.8,
-    backgroundColor: 'grey',
+    height: "70%",
+    width: Dimensions.get("window").width * 0.8,
+    backgroundColor: "grey",
     borderRadius: 10,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   sendButtontext: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 18,
-    color: 'black',
-  }
-})
-
+    color: "black",
+  },
+});
 
 const mapStateToProps = (state) => ({
   uri: state.newPost.uri,
-})
+});
 
-
-export default withNavigation(connect(
-  mapStateToProps,
-)(AddVideoScreen));
+export default withNavigation(connect(mapStateToProps)(AddVideoScreen));
