c41ab2cf3e9cda7ec21567310c78c3983a6dc598
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 11:59:35 +0000

    :construction: update ChatInboxScreen

--- screens/ChatInboxScreen.js
+++ screens/ChatInboxScreen.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { withNavigation } from 'react-navigation';
-
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { withNavigation } from "react-navigation";
 
 class ChatInboxScreen extends React.Component {
   constructor(props) {
@@ -13,14 +12,14 @@ class ChatInboxScreen extends React.Component {
       <View style={styles.container}>
         <Text>Chat Inbox Screen</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
-})
+  },
+});
 
 export default withNavigation(ChatInboxScreen);
