cfabcc7c1d1e1bcb411a8b04a5c0780b1a2f48ab
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 12:07:13 +0000

    :sparkles: create VideoEditHeader

---
+++ components/VideoEdit/VideoEditHeader.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useNavigation } from '@react-navigation/native';
+import { View, StyleSheet, Dimensions, Pressable } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+const { height } = Dimensions.get("window");
+
+const VideoEditHeader = ({}) => {
+  const navigation = useNavigation();
+
+  const handlePressGoBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.goBack} onPress={handlePressGoBack}>
+        <Ionicons name="ios-arrow-back" size={24} color="white" />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    elevation: 4,
+    zIndex: 100,
+    height: 0.1 * height,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  goBack: {
+    height: 36,
+    width: 36,
+    borderRadius: 10,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default VideoEditHeader;
