e2cf77a811d21aee1c9aa851061edfd90bf79056
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 12:05:13 +0000

    :lipstick: update ListPreview

--- components/Lists/ListPreview.js
+++ components/Lists/ListPreview.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 import {
   View,
   Text,
   StyleSheet,
   Dimensions,
   TouchableOpacity,
-} from 'react-native';
-import { connect } from 'react-redux';
+} from "react-native";
+import { connect } from "react-redux";
 
-import { makeGetListName } from '../../ducks/listsSlice';
+import { makeGetListName } from "../../ducks/listsSlice";
 
-
-const margin = 15;
+const margin = 7;
 const borderRadius = 15;
-const width = (Dimensions.get('window').width - 4 * 15) / 2;
-
+const width = (Dimensions.get("window").width - 4 * margin) / 2;
 
 class ListPreview extends React.Component {
   constructor(props) {
@@ -22,49 +20,48 @@ class ListPreview extends React.Component {
   }
 
   onPress = () => {
-    this.props.navigation.push(
-      "VideoStack",
-      {
-        screen: "List",
-        params: { list: { id: this.props.id } }
-      }
-    )
+    this.props.navigation.push("VideoStack", {
+      screen: "List",
+      params: { list: { id: this.props.id } },
+    });
   };
 
   render() {
     return (
-      <TouchableOpacity key={this.props.id} style={styles.container} onPress={this.onPress}>
+      <TouchableOpacity
+        key={this.props.id}
+        style={styles.container}
+        onPress={this.onPress}
+      >
         <Text style={styles.name}>{this.props.name}</Text>
       </TouchableOpacity>
-    )
+    );
   }
-};
+}
 
 const styles = {
   container: {
-    backgroundColor: '#d3d3d3',
+    backgroundColor: "#d3d3d3",
     height: width * 1.77,
     width,
-    margin ,
+    margin,
     padding: 15,
     borderRadius,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
+    flexDirection: "column",
+    justifyContent: "flex-end",
   },
   name: {
     fontSize: 24,
-  }
+  },
 };
-
 
 const makeMapStateToProps = (state) => {
   const getListName = makeGetListName();
   return function mapStateToProps(state, ownProps) {
     return {
       name: getListName(state, { listId: ownProps.id }),
-    }
-  }
+    };
+  };
 };
-
 
 export default connect(makeMapStateToProps)(ListPreview);
