86d2b2560bd022c1463fe7548a7442b6ac831f25
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-12 22:58:13 +0000

    :sparkles: add CreateAccountScreen to AuthStack navigator

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -2,14 +2,16 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import SignInScreen from "../screens/SignInScreen";
+import CreateAccountScreen from "../screens/CreateAccountScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true }}>
-    <Stack.Screen name="SignUp" component={SignUpScreen} />
     <Stack.Screen name="SignIn" component={SignInScreen} />
+    <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+    <Stack.Screen name="SignUp" component={SignUpScreen} />
   </Stack.Navigator>
 );
 
