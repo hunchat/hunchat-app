822a947f9344d992405695fe3104791d2134bb96
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-22 14:12:07 +0000

    :sparkles: create onboardingAddVideoBioSlice

---
+++ ducks/onboardingAddVideoBioSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const onboardingAddVideoBioSlice = createSlice({
+  name: "onboardingAddVideoBio",
+  initialState: {
+    uri: null, // URI of camera video
+    previewUri: null, // URI of preview video
+  },
+  reducers: {
+    setOnboardingVideoBio: (state, action) => {
+      state.uri = action.payload;
+    },
+    setPreviewOnboardingVideoBio: (state, action) => {
+      state.previewUri = action.payload;
+    },
+  },
+});
+
+export const { setOnboardingVideoBio, setPreviewOnboardingVideoBio } = onboardingAddVideoBioSlice.actions;
+
+export default onboardingAddVideoBioSlice.reducer;
