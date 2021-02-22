00d96e9a06c31e4bdc38636f5c121749b343d518
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-22 14:12:19 +0000

    :sparkles: update appReducer

--- ducks/index.js
+++ ducks/index.js
@@ -2,17 +2,21 @@ import { combineReducers } from "redux";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import authReducer from "./authSlice";
+import editProfileReducer from "./editProfileSlice";
 import listsReducer from "./listsSlice";
 import newPostReducer from "./newPostSlice";
 import usersReducer from "./usersSlice";
 import postsReducer from "./postsSlice";
+import onboardingAddVideoBioReducer from "./onboardingAddVideoBioSlice";
 
 const appReducer = combineReducers({
   auth: authReducer,
+  editProfile: editProfileReducer,
   lists: listsReducer,
   newPost: newPostReducer,
   posts: postsReducer,
   users: usersReducer,
+  onboardingAddVideoBio: onboardingAddVideoBioReducer,
 });
 
 export const rootReducer = (state, action) => {
