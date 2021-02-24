c474b51573558bff41c19462ed41c0c479f3de4a
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 22:41:35 +0000

    :sparkles: update ProfileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,25 +1,88 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, StyleSheet, Animated, Dimensions } from "react-native";
+import { connect } from "react-redux";
 import { withNavigation } from "react-navigation";
+import { getStatusBarHeight } from "react-native-status-bar-height";
 
-class ProfileScreen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import Profile, { ProfileHeader } from "../components/Profile";
+import { PostPreview } from "../components/Post";
+import {
+  makeGetUser,
+  retrieveUserThunk,
+} from "../ducks/usersSlice";
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Profile Screen</Text>
-      </View>
-    );
-  }
+const { height } = Dimensions.get("window");
+export const PROFILE_HEIGHT = height - getStatusBarHeight();
+
+function ProfileScreen({
+  navigation,
+  route,
+  postsIds,
+  retrieveUserThunk,
+}) {
+
+  useEffect(() => {
+      retrieveUserThunk(route.params.userId);
+    },
+    [route.params.userId],
+  );
+
+  const keyExtractor = (item) => item;
+
+  const renderItem = ({ item }) => <PostPreview id={item} />;
+
+  return (
+    <Animated.View style={styles.container}>
+      <ProfileHeader />
+      <Animated.FlatList
+        scrollEventThrottle={1}
+        data={postsIds}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.flatList}
+        contentContainerStyle={styles.list}
+        numColumns={2}
+        columnWrapperStyle={styles.column}
+        // onScroll={handleScroll}
+        bounces={false}
+        ListHeaderComponent={
+          <Profile userId={route.params.userId}/>
+        }
+        ListHeaderComponentStyle={{ height: PROFILE_HEIGHT }}
+      />
+    </Animated.View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "black",
+  },
+  flatList: {
+    flex: 1,
+  },
+  list: {
+    justifyContent: "space-around",
+  },
+  column: {
+    flexShrink: 1,
   },
 });
 
-export default withNavigation(ProfileScreen);
+const makeMapStateToProps = () => {
+  const mapStateToProps = (state) => {
+    return {
+      postsIds: ["1", "2", "3", "4", "5"],
+    };
+  };
+  return mapStateToProps;
+};
+
+const mapDispatchToProps = {
+  retrieveUserThunk,
+};
+
+export default withNavigation(
+  connect(makeMapStateToProps, mapDispatchToProps)(ProfileScreen)
+);
