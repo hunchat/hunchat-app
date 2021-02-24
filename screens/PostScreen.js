72c0bfe4fed918cee45cc8007a5edfc4e42f71b5
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 22:41:18 +0000

    :sparkles: update PostScreen

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { withNavigation } from "react-navigation";
-import { connect } from "react-redux";
-import { View } from "react-native";
+import { View, Pressable, StyleSheet, Dimensions } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import Post from "../components/Post";
 import { makeGetPost } from "../ducks/postsSlice";
+
+const { height } = Dimensions.get("window");
 
 class PostScreen extends React.Component {
   constructor(props) {
@@ -14,20 +16,43 @@ class PostScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Post {...this.props.post} navigation={this.props.navigation} />
+        <View style={styles.header}>
+          <Pressable
+            style={styles.goBack}
+            onPress={() => this.props.navigation.goBack()}
+          >
+            <Ionicons name="ios-arrow-back" size={24} color="white" />
+          </Pressable>
+        </View>
+        <Post id={this.props.route.params.postId} />
       </View>
     );
   }
 }
 
-const makeMapStateToProps = (state) => {
-  const getPost = makeGetPost();
-  return function mapStateToProps(state, ownProps) {
-    let post = getPost(state, { postId: ownProps.route.params.post.id });
-    return {
-      post: post,
-    };
-  };
-};
+const styles = StyleSheet.create({
+  header: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    elevation: 4,
+    zIndex: 100,
+    height: 0.1 * height,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  goBack: {
+    height: 36,
+    width: 36,
+    borderRadius: 10,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default withNavigation(connect(makeMapStateToProps)(PostScreen));
+export default withNavigation(PostScreen);
