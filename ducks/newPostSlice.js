9850e38fcfd1826d2f2c21fceedf9f68ddfbd404
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 22:33:59 +0000

    :sparkles: update newPostSlice

--- ducks/newPostSlice.js
+++ ducks/newPostSlice.js
@@ -1,17 +1,63 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export function postCreateThunk(data) {
+  return function (dispatch, getState) {
+    dispatch(setAddPostFormSubmittingStatus("pending"));
+
+    const PostsService = require("../services/api/posts/PostsService");
+    const postsService = new PostsService();
+
+    postsService
+      .create(data)
+      .then((response) => {
+        dispatch(setAddPostFormSubmittingStatus("fulfilled"));
+        dispatch(setShowSucessPopUp(true));
+
+        // Reset form
+        dispatch(setNewPostVideo(null))
+        dispatch(setNewPostCommentTo(null))
+      })
+      .catch((error) => {
+        dispatch(setAddPostFormSubmittingStatus("rejected"));
+      })
+  }
+};
+
 const newPostSlice = createSlice({
   name: "newPost",
   initialState: {
-    uri: null,
+    video: null,
+    previewVideo: null,
+    commentTo: null,
+    formSubmittingStatus: "inactive", // 'inactive' || 'pending' || 'fulfilled' || 'rejected'
+    formError: null,
+    showSucessPopUp: false,
   },
   reducers: {
-    setVideoUri: (state, action) => {
-      state.uri = action.payload;
+    setNewPostVideo: (state, action) => {
+      state.video = action.payload;
     },
+    setNewPostCommentTo: (state, action) => {
+      state.commentTo = action.payload;
+    },
+    setAddPostFormSubmittingStatus: (state, action) => {
+      state.formSubmittingStatus = action.payload;
+    },
+    setAddPostFormError: (state, action) => {
+      state.formError = action.payload;
+    },
+    setShowSucessPopUp: (state, action) => {
+      state.showSucessPopUp = action.payload;
+    }
   },
 });
 
-export const { setVideoUri } = newPostSlice.actions;
+export const {
+  setNewPostVideo,
+  setNewPostCommentTo,
+  setAddPostFormSubmittingStatus,
+  setAddPostFormError,
+  setShowSucessPopUp,
+} = newPostSlice.actions;
 
 export default newPostSlice.reducer;
