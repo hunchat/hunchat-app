5b98d98979cc105c31c78a2dcd15041d5ee5582f
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 22:39:10 +0000

    :sparkles: update App

--- App.js
+++ App.js
@@ -1,37 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, SafeAreaView, StatusBar, Platform, Linking } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  Linking,
+} from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import AppLoading from 'expo-app-loading';
 import Dialog from "react-native-dialog";
+import { decode, encode } from "base-64";
 
+import checkIfFirstLaunch from "./utils/checkIfFirstLaunch";
+import loadAuthData from "./utils/loadAuthData";
 import { AppStack, navigationRef } from "./navigation";
 
-function App() {
-  const [visible, setVisible] = useState(
-    Platform.OS === 'ios' && Platform.Version === '14.0' ? true : false
-  );
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
-  const onPressUpdate = () => {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFirstLaunch: false,
+      appIsReady: false,
+      visible: Platform.OS === "ios" && Platform.Version === "14.0" ? true : false,
+    }
+  };
+
+  /**
+   * Method that serves to load resources and make API calls
+   */
+  prepareResources = async () => {
+    const authData = await loadAuthData();
+    const isFirstLaunch = await checkIfFirstLaunch();
+    this.setState({ isFirstLaunch });
+  };
+
+  onPressUpdate = () => {
     Linking.openURL("App-prefs:root=General");
   };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer ref={navigationRef}>
+  render() {
+    if (!this.state.appIsReady) {
+      return (
+        <AppLoading
+          startAsync={this.prepareResources}
+          onFinish={() => this.setState({ appIsReady: true })}
+          onError={console.warn}
+          autoHideSplash={true}
+        />
+      );
+    }
 
-      {/* Alert of problem with iOS 14.0 */}
-      {/* iOS 14.0 causes a break on expo-av.Video. See https://github.com/expo/expo/issues/10249 */}
-      <Dialog.Container visible={visible}>
-        <Dialog.Title>Please update your software</Dialog.Title>
-        <Dialog.Description>
-          You are using iOS 14.0. Hunchat doesn't like this version. Make Hunchat happy and update.
-        </Dialog.Description>
-        <Dialog.Button label="Update" onPress={onPressUpdate} />
-      </Dialog.Container>
+    return (
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer ref={navigationRef}>
+          {/* Alert of problem with iOS 14.0 */}
+          {/* iOS 14.0 causes a break on expo-av.Video. See https://github.com/expo/expo/issues/10249 */}
+          <Dialog.Container visible={this.state.visible}>
+            <Dialog.Title>Please update your software</Dialog.Title>
+            <Dialog.Description>
+              You are using iOS 14.0. Hunchat doesn't like this version. Make
+              Hunchat happy and update.
+            </Dialog.Description>
+            <Dialog.Button label="Update" onPress={this.onPressUpdate} />
+          </Dialog.Container>
 
-        <AppStack />
-      </NavigationContainer>
-    </SafeAreaView>
-  );
+          <AppStack
+            authRefreshToken={this.props.authRefreshToken}
+            currentUserId={this.props.currentUserId}
+            isFirstLaunch={this.state.isFirstLaunch}
+          />
+
+        </NavigationContainer>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -41,4 +92,9 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+const mapStateToProps = state => ({
+  authRefreshToken: state.auth.refreshToken,
+  currentUserId: state.users.currentUserId,
+});
+
+export default connect(mapStateToProps)(App);
