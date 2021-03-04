9d39d4f2efdb499beb66e7db6bcfc3d1ece19885
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-04 22:27:50 +0000

    :sparkles: create ListsSelector

---
+++ components/Lists/ListsSelector.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { connect } from "react-redux";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Pressable,
+  Dimensions,
+  Text,
+} from "react-native";
+
+const width = Dimensions.get("window");
+
+const LIST_TAG_HEIGHT = 30;
+const LISTS_SELECTOR_PADDING_VERTICAL = 10;
+export const LISTS_SELECTOR_HEIGHT =
+  LIST_TAG_HEIGHT + 2 * LISTS_SELECTOR_PADDING_VERTICAL;
+
+const lists = [
+  {
+    id: "123",
+    name: "All Lists",
+  },
+  {
+    id: "234",
+    name: "Design",
+  },
+  {
+    id: "345",
+    name: "Sports",
+  },
+  {
+    id: "456",
+    name: "Coding",
+  },
+  {
+    id: "567",
+    name: "Baking",
+  },
+  {
+    id: "678",
+    name: "Architecture",
+  },
+  {
+    id: "789",
+    name: "Science",
+  },
+  {
+    id: "890",
+    name: "Politics",
+  },
+];
+
+const ListTag = ({ name }) => {
+  return (
+    <Pressable
+      style={[
+        listTagStyles.container,
+        { backgroundColor: name === "All Lists" ? "white" : "black" },
+      ]}
+    >
+      <Text style={{ color: name === "All Lists" ? "black" : "white" }}>
+        {name}
+      </Text>
+    </Pressable>
+  );
+};
+
+const listTagStyles = StyleSheet.create({
+  container: {
+    height: LIST_TAG_HEIGHT,
+    marginHorizontal: 3,
+    borderRadius: 10,
+    paddingVertical: 3,
+    paddingHorizontal: 7,
+    justifyContent: "center",
+  },
+});
+
+const ListsSelector = ({ selectedList }) => {
+  const renderItem = ({ item }) => {
+    const isSelected = item.name === selectedList;
+    return <ListTag name={item.name} isSelected={isSelected} />;
+  };
+
+  const keyExtractor = (item) => item.id;
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={lists}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    backgroundColor: "black",
+    paddingVertical: LISTS_SELECTOR_PADDING_VERTICAL,
+    paddingHorizontal: 5,
+  },
+});
+
+export default ListsSelector;
--- components/Lists/index.js
+++ components/Lists/index.js
@@ -3,5 +3,14 @@ import ListPostsHeader from "./ListPostsHeader";
 import ListPostsPreview from "./ListPostsPreview";
 import ListPreview from "./ListPreview";
 import Lists from "./Lists";
+import ListsSelector, { LISTS_SELECTOR_HEIGHT } from "./ListsSelector";
 
-export { ListPosts, ListPostsHeader, ListPostsPreview, ListPreview, Lists };
+export {
+  ListPosts,
+  ListPostsHeader,
+  ListPostsPreview,
+  ListPreview,
+  Lists,
+  ListsSelector,
+  LISTS_SELECTOR_HEIGHT,
+};
