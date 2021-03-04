68dfb62a729f021cff2073edc07b04d0d36bc225
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-04 23:13:28 +0000

    :art: run prettier

--- components/Post/index.js
+++ components/Post/index.js
@@ -1,7 +1,16 @@
 import Post, { POST_HEIGHT } from "./Post";
-import PostPreview from "./PostPreview";
+import PostPreview, {
+  POST_PREVIEW_HEIGHT,
+  POST_PREVIEW_WIDTH,
+} from "./PostPreview";
 import PostsList from "./PostsList";
 
-export { POST_HEIGHT, PostPreview, PostsList };
+export {
+  POST_HEIGHT,
+  PostPreview,
+  POST_PREVIEW_HEIGHT,
+  POST_PREVIEW_WIDTH,
+  PostsList,
+};
 
 export default Post;
